4c5c72c6bbda79e2ab4cc75522b7a1cb96bdedef
lixuannan <codingcow@tuta.io>
2022-11-14 15:51:53 +0800

    Remove push.sh

--- push.sh
+++
@@ -1,7 +0,0 @@
-cd /Users/meizhenchen/Desktop/Project/oiclass-answers/ || exit
-git add .
-# shellcheck disable=SC2209
-date_time=$(date)
-# shellcheck disable=SC2154
-git commit -m "Update in $date_time"
-git push -u origin main
